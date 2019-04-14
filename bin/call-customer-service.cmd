1aefcac39ff10ce6c77a8b753259205a26f90627
Stanislav Deviatov <stanislav_deviatov@epam.com>
2019-04-14 14:08:33 +0300

    call-customer-service.cmd was updated

--- bin/call-customer-service.cmd
+++ bin/call-customer-service.cmd
@@ -1,7 +1,2 @@
 @echo off
-:begin
-	
-curl http://127.0.0.1:8001/api/v1/namespaces/demo/services/customer:8080/proxy/
-powershell Start-Sleep -m 500
-	
-goto begin
+powershell -ExecutionPolicy ByPass -File call-customer-service.ps1
